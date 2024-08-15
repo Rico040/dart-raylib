69197c3119f8dfe60429030c25c8a26db79dc38c
Rico040 <93081680+Rico040@users.noreply.github.com>
2024-08-15 23:07:57 +0900

    Remove unused import

--- lib/src/modules/core/window.dart
+++ lib/src/modules/core/window.dart
@@ -1,5 +1,4 @@
 import 'package:raylib/raylib.dart';
-import 'package:raylib/src/generated_bindings.dart' as raylib;
 import 'package:raylib/src/library.dart';
 import 'package:raylib/src/utils/string.dart' as string;
 
